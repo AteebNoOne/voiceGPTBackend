0e86917e32b5162044c24c09b02547bc4d3ed2b9
Ateeb Ur Rahmaan <104502014+AteebNoOne@users.noreply.github.com>
2024-01-21 08:10:28 +0500

    final

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ import os
 from datetime import datetime
 import google.generativeai as genai
 from gtts import gTTS
-
+from dotenv import load_dotenv  
 
 app = Flask(__name__)
 CORS(app)
 
 
-from dotenv import load_dotenv  # Add this line
+
 
 # Load environment variables from .env file
 load_dotenv()
